decab84fd611c8ede886ae3ed188f83f83ae3fbb
kadai-2 <45530566+mina373737@users.noreply.github.com>
2018-12-21 23:45:19 -0700

    Add files via upload

---
+++ kansuu2.php
@@ -0,0 +1,17 @@
+<?php
+// 課題４
+// 配列の中で一番大きい値を返す max_array という関数を実装
+function max_array($arr){
+  $max_number=$arr[0];
+  //$arrという箱から$aを取り出す
+  foreach($arr as $a){
+//$aは0から始まり、$arrよりも小さく、1ずつ増える
+    for($a=0;$a<$arr;$a++);
+    //もし$aが$arrという箱に入った数値よりも大きければ
+}
+    if($a<$arr){
+//$max_number（最大値）を返す
+  return $max_number
+}
+
+ ?>
