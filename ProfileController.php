1ae75cb3563256ace5329d52523cc3eb08a8336d
kadai-2 <45530566+mina373737@users.noreply.github.com>
2018-12-28 23:33:39 -0700

    Add files via upload

--- ProfileController.php
+++ ProfileController.php
@@ -17,3 +17,16 @@ class ProfileController extends Controller
       return redirect('admin/profile/edit');
     }
 }
+
+//課題4
+
+// 【応用】 前章でAdmin/ProfileControllerを作成し、edit Actionを追加しました。 web.phpを編集してadmin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix'=>'admin'],function(){
+  Route::get('news/create','Admin\NewsController@add');
+  Route::get('admin/profile/edit','Admin\ProfileController@edit Action')
+});
