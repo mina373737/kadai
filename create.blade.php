07df120689d5a26d57e2a54d74df8d52723a7f5f
kadai-2 <45530566+mina373737@users.noreply.github.com>
2019-01-25 04:07:49 -0700

    Add files via upload

---
+++ create.blade.php
@@ -0,0 +1,56 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'Myプロフィールの登録'を埋め込む --}}
+@section('title', 'Myプロフィールの登録')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名(name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                       <label class="col-md-2" for="title">性別</label>
+                       <div class="col-md-10">
+                           <input type="radio" name="gender" value="male" checked="checked">男性
+                           <input type="radio" name="gender" value="female">女性
+                       </div>
+                     </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">趣味(hoby)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介(introduction)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
